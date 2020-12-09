c434752c40c6d6559e8ba88f5c98291553b84d33
Travis Milner <travismilner1@gmail.com>
2020-12-09 11:57:45 -0600

    created an on change event that when selecting a fruit or veggie it will change the state of my empty array. Copied that empty array to make sure it was keeping track of state changes. Rendered what was in the array to each grid plot based on if an item at specific indexes was populated

---
+++ src/Plants/PlantGrid.js
@@ -0,0 +1,47 @@
+import React from "react"
+
+
+
+
+
+
+
+export const PlantGrid = ({plantArr}) => {
+    console.log(plantArr)
+
+const obj = {
+    grid1: plantArr[0]  ? plantArr[0] : "",
+    grid2: plantArr[1]  ? plantArr[1] : "",
+    grid3: plantArr[2]  ? plantArr[2] : "",
+    grid4: plantArr[3]  ? plantArr[3] : "",
+    grid5: plantArr[4]  ? plantArr[4] : "",
+    grid6: plantArr[5]  ? plantArr[5] : "",
+    grid7: plantArr[6]  ? plantArr[6] : "",
+    grid8: plantArr[7]  ? plantArr[7] : "",
+    grid9: plantArr[8]  ? plantArr[8] : ""
+}
+console.log(obj)
+
+
+    return (
+        <>
+            <div className = "grid">
+                <div className = "grid__item" id= "item--1">{obj.grid1}</div>
+                <div className = "grid__item" id= "item--2">{obj.grid2}</div>
+                <div className = "grid__item" id= "item--3">{obj.grid3}</div>
+                <div className = "grid__item" id= "item--4">{obj.grid4}</div>
+                <div className = "grid__item" id= "item--5">{obj.grid5}</div>
+                <div className = "grid__item" id= "item--6">{obj.grid6}</div>
+                <div className = "grid__item" id= "item--7">{obj.grid7}</div>
+                <div className = "grid__item" id= "item--8">{obj.grid8}</div>
+                <div className = "grid__item" id= "item--9">{obj.grid9}</div>
+                <button>Save Grid</button>
+                
+            </div>
+
+
+
+        </>
+
+    )
+}
--- src/Plants/PlantList.js
+++ src/Plants/PlantList.js
@@ -1,24 +1,32 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { PlantContext } from "./PlantProvider"
 import { Plant } from "./Plant"
 import "./Plant.css"
+import { PlantGrid } from "./PlantGrid"
 
 
 export const PlantList = () => {
     const { plants, getPlants } = useContext(PlantContext)
-
+    const [plant, setplant ] = useState([])
     useEffect(() => {
         getPlants()
     }, [])
-
+let plantArr = []
     return (
         <div className = "plants">
-            <select className = "plantDrop">
-                <option value = "0">Please select a crop...</option>
+            <select className = "plantDrop" onChange= {(e) => {
+                console.log("hello")
+                plantArr = plant.slice()
+                plantArr.push(e.target[e.target.options.selectedIndex].label)
+                setplant(plantArr)
+            
+            }}>
+                <option value = "0" >Please select a crop...</option>
             {
                 plants.map(pla => <Plant key = {pla.id} plant = {pla} />)
             }
             </select>
+            <PlantGrid plantArr = {plant} />
         </div>
     )
 }
